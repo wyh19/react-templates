6cf257364ad898ea228c79c13d9f5f70ece82768
wyh19 <wyh_19@163.com>
2018-06-05 10:00:31 +0800

    搭建前端的目录

---
+++ react-node-mongo/src/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by 30113 on 2018/5/5.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {AppContainer} from 'react-hot-loader'
+import App from './App'
+
+
+const root = document.getElementById('root')
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,
+        root
+    )
+}
+
+render(App)
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default
+        render(NextApp)
+    })
+}
