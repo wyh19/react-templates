52dd1aa8dfb46f9216b54295f42917803d53575a
wyh19 <wyh_19@163.com>
2018-06-11 14:13:34 +0800

    搭建登录与注册的页面

--- react-node-mongo/src/pages/login/Login.jsx
+++ react-node-mongo/src/pages/login/Login.jsx
@@ -4,11 +4,33 @@
 import React from 'react'
 
 class Login extends React.Component{
-    render(){
+    constructor(props){
+        super(props)
+        this.handleLogin = this.handleLogin.bind(this)
+        this.gotoRegister = this.gotoRegister.bind(this)
+    }
+    handleLogin(){
 
+    }
+    gotoRegister(){
+        this.props.history.push('/register')
+    }
+    render(){
         return(
             <div >
-                login
+                <h2>登录页</h2>
+                <div>
+                    <label>用户名</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>密码</label>
+                    <input type="password"/>
+                </div>
+                <div>
+                    <button onClick={this.handleLogin}>登录</button>
+                    <button onClick={this.gotoRegister}>注册</button>
+                </div>
             </div>
         )
     }
