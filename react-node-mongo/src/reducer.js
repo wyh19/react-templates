957b335f282b66b3ce1f9b0c2dad1bcb309bf7a2
wyh19 <wyh_19@163.com>
2018-06-11 14:46:11 +0800

    添加user的reducer

--- react-node-mongo/src/reducer.js
+++ react-node-mongo/src/reducer.js
@@ -2,5 +2,6 @@
  * Created by 30113 on 2018/6/6.
  */
 import {combineReducers} from 'redux'
+import {user} from './redux/user'
 
-export default combineReducers({})
+export default combineReducers({user})
