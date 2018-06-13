8d788c5cb9172c0afe2f0324740b17773d08de99
wyh19 <wyh_19@163.com>
2018-06-13 17:20:55 +0800

    添加home页

---
+++ react-node-mongo/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+/**
+ * Created by 30113 on 2018/6/13.
+ */
+import React from 'react'
+
+class Home extends React.Component{
+    render(){
+        return <h2>home</h2>
+    }
+}
+
+export default Home
